e9d5e3b01756a38c6809b8984ddfe398ba26c43b
Killian Blais <contact@killianblais.fr>
2021-01-18 13:22:47 +0100

    Rework tests and add tests for the new way to call components
    Separate tests between existing syntax and the new syntax. Also test if the exception is thrown when the syntax is not respected.

---
+++ tests/RenderTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Enflow\LivewireTwig\Test;
+
+use Livewire\Component;
+use Livewire\Livewire;
+
+class RenderTest extends TestCase
+{
+    public function test_name_type_component_correctly_renders()
+    {
+        Livewire::component('counter', Counter::class);
+
+        $rendered = view('name-type-test')->render();
+
+        $this->assertStringContainsString('[wire\:loading]', $rendered); // Styles
+        $this->assertStringContainsString('window.livewire', $rendered); // Scripts
+        $this->assertStringContainsString('increment', $rendered); // Counter component
+        $this->assertStringContainsString('Lorem ipsum!', $rendered); // Counter component title
+    }
+
+    public function test_string_type_component_correctly_renders()
+    {
+        Livewire::component('counter', Counter::class);
+
+        $rendered = view('string-type-test')->render();
+
+        $this->assertStringContainsString('[wire\:loading]', $rendered); // Styles
+        $this->assertStringContainsString('window.livewire', $rendered); // Scripts
+        $this->assertStringContainsString('increment', $rendered); // Counter component
+        $this->assertStringContainsString('Lorem ipsum!', $rendered); // Counter component title
+    }
+
+    public function test_invalid_type_throws_exception()
+    {
+        $this->expectException(\ErrorException::class);
+        $this->expectExceptionMessage('Unexpected token "number" of value "63" ("name" or "string" expected).');
+        Livewire::component('counter', Counter::class);
+
+        $rendered = view('invalid-type-test')->render();
+    }
+}
+
+class Counter extends Component
+{
+    public $count = 3;
+    public $title = null;
+
+    public function render()
+    {
+        return view('components.counter');
+    }
+}
