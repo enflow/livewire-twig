b2770c17d9aa95fcfd27574f35ca5a9493fef69f
mbardelmeijer <mbardelmeijer@users.noreply.github.com>
2024-03-15 12:11:55 +0000

    Fix styling

--- src/LivewireTwigServiceProvider.php
+++ src/LivewireTwigServiceProvider.php
@@ -11,6 +11,6 @@ class LivewireTwigServiceProvider extends ServiceProvider
     {
         $this->app->afterResolving('twig', fn () => $this->app['twig']->addExtension(new LivewireExtension()));
 
-        $this->app->extend(DeterministicBladeKeys::class, fn() => new NoneDeterministicTwigKeys());
+        $this->app->extend(DeterministicBladeKeys::class, fn () => new NoneDeterministicTwigKeys());
     }
 }
